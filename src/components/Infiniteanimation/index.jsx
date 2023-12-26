a428a9521d686c8a160de13420a151ac598998a7
malikabbasov <https://github.com/MalikAbbasov/frontend.git>
2023-12-26 19:52:35 +0400

    slideri yazdim

--- src/components/Infiniteanimation/index.jsx
+++ src/components/Infiniteanimation/index.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import "./infinite.scss"
+import './infinite.scss'
 
-function Infiniteanimation() {
+const Infiniteanimation = () => {
     return (
-        <div>
-            <div id="infinite">
-                <div class="slider">
-                    <div class="slide-track">
-                        <div class="slide">
-                            <h1>#salam</h1>
+        <div className="slider">
+            <div className="wrapper">
+                <div className="slider">
+                    <div className="slide-track">
+                        <div className="slide">
+                            <h1>salam</h1>
                         </div>
-                        <div class="slide">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                        <div className="slide">
+                            <h1>sagol</h1>
                         </div>
-                        <div class="slide">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                        <div className="slide">
+                            <h1>salam</h1>
                         </div>
-                        <div class="slide">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                        <div className="slide">
+                            <h1>sagol</h1>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
-export default Infiniteanimation
+export default Infiniteanimation;
